da409d94a2d8baa5fe74c69efa95fc3627ee57ad
Guilherme609 <guilherme.moreira@rede.ulbra.br>
2024-09-12 16:48:09 -0300

    versão 2
    foi adicionado:
    - o botão de reset
    - o alerta de GAME OVER
    - a alteração da cor do vencedor

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,4 +1,4 @@
-const tic_tac_toe = {
+/*const tic_tac_toe = {
     board: ['', '', '', '', '', '', '', '', ''],
     simbols: {
         opitions: ['X', 'O'],
@@ -10,6 +10,16 @@ const tic_tac_toe = {
     },
     conteiner_element: null,
     gameover: false,
+    winning_sequences: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ],
 
     init: function (conteiner) {
         this.conteiner_element = conteiner;
@@ -20,10 +30,37 @@ const tic_tac_toe = {
         if (this.board[position] === '') {
             this.board[position] = this.simbols.opitions[this.simbols.turn_index];
             this.draw();
-
-            this.simbols.charge();
+            let winning_sequences_index = this.check_winning_sequences(this.simbols.opitions[this.simbols.turn_index]);
+            if (this.check_winning_sequences >= 0) {
+                this.game_is_over();
+            } else {
+                this.simbols.charge();
+            }
+            return true;
+        } else {
+            return false;
         }
+    },
 
+    game_is_over: function () {
+        this.gameover = true;
+    },
+
+    start: function(){
+        this.board.fill('');
+        this.draw();
+        this.gameover = false;
+    },
+
+    check_winning_sequences: function () {
+        for (i in this.winning_sequences) {
+            if (this.board[this.winning_sequences[i][0]] == simbol &&
+                this.board[this.winning_sequences[i][1]] == simbol &&
+                this.board[this.winning_sequences[i][2]] == simbol) {
+                return i;
+            }
+        }
+        return -1;
     },
 
     draw: function () {
@@ -34,4 +71,106 @@ const tic_tac_toe = {
         }
         this.conteiner_element.innerHTML = content;
     }
+};*/
+const tic_tac_toe = {
+
+    // ATTRIBUTES
+    board: ['','','','','','','','',''],
+    symbols: {
+                options: ['O','X'],
+                turn_index: 0,
+                change(){
+                    this.turn_index = ( this.turn_index === 0 ? 1:0 );
+                }
+            },
+    container_element: null,
+    gameover: false,
+    winning_sequences: [
+                        [0,1,2],
+                        [3,4,5],
+                        [6,7,8],
+                        [0,3,6],
+                        [1,4,7],
+                        [2,5,8],
+                        [0,4,8],
+                        [2,4,6]
+                    ],
+
+    // FUNCTIONS
+    init(container) {
+        this.container_element = container;
+    },
+
+    make_play(position) {
+        if (this.gameover || this.board[position] !== '') return false;
+
+        const currentSymbol = this.symbols.options[this.symbols.turn_index];
+        this.board[position] = currentSymbol;
+        this.draw();
+
+        const winning_sequences_index = this.check_winning_sequences(currentSymbol);
+        if (this.is_game_over()){
+            this.game_is_over();
+        }
+        if (winning_sequences_index >= 0) {
+            this.game_is_over();
+            this.stylize_winner_sequence(this.winning_sequences[winning_sequences_index]);
+        } else {
+            this.symbols.change();
+        }
+
+        return true;
+    },
+
+    stylize_winner_sequence(winner_sequence) {
+        winner_sequence.forEach((position) => {
+          this
+            .container_element
+            .querySelector(`div:nth-child(${position + 1})`)
+            .classList.add('winner');
+        });
+      },
+
+    check_winning_sequences(symbol) {
+
+        for ( i in this.winning_sequences ) {
+            if (this.board[ this.winning_sequences[i][0] ] == symbol  &&
+                this.board[ this.winning_sequences[i][1] ] == symbol &&
+                this.board[ this.winning_sequences[i][2] ] == symbol) {
+                console.log('winning sequences INDEX:' + i);
+                alert("GAME OVER")
+                return i;
+            }
+        };
+        return -1;
+    },
+
+    game_is_over() {
+        this.gameover = true;
+        console.log('GAME OVER');
+    },
+
+    is_game_over() {
+        return !this.board.includes('');
+    },
+
+    start() {
+        this.board.fill('');
+        this.draw();
+        this.gameover = false;       
+    },
+
+    restart() {
+        if (this.is_game_over() || this.gameover) {
+            this.start();
+            console.log('this game has been restarted!')
+        } else if (confirm('Are you sure you want to restart this game?')) {
+            this.start();
+            console.log('this game has been restarted!')
+        }
+    },
+
+    draw() {
+        this.container_element.innerHTML = this.board.map((element, index) => `<div onclick="tic_tac_toe.make_play('${index}')"> ${element} </div>`).reduce((content, current) => content + current);
+    },
 };
